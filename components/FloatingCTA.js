54c384f96039a0bb2a08b227dcba056bbc434086
amnaduny <amnaduniakhara@gmail.com>
2025-05-09 22:45:15 +0200

    edit floatingCTA

--- components/FloatingCTA.js
+++ components/FloatingCTA.js
@@ -1,21 +1,17 @@
-import { FaWhatsapp } from 'react-icons/fa';
-import Link from 'next/link';
-import Image from 'next/image';
-
 export default function FloatingCTA() {
   return (
-    <div className="floating-cta">
+    <div className="floating-cta responsive-text" data-aos="zoom in">
       <p className="cta-text">Hi!👋<br />
-      Kalau kamu butuh bantuan, Hubungi kami via WhatsApp ya !</p>
+        Kalau kamu butuh bantuan, Hubungi kami via WhatsApp ya !</p>
       <a
-        href="https://wa.me/6282337506356?text=Halo%20Lernen%20Indonesia!"
+        href="https://wa.me/+6282337506356?text=Halo%20Lernen%20Indonesia!"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img
           src="/assets/social-media/whatsapp2.svg"
           alt="WhatsApp"
-          width={55} 
+          width={55}
           height={55}
         />
       </a>
